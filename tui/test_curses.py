c471f1e82ff5a8e67917b48aff67303053972801
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2025-08-14 17:45:52 +0200

    updated tui_curses

--- tui/test_curses.py
+++ tui/test_curses.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+from argparse import ArgumentParser
 import curses
 from curses.textpad import Textbox, rectangle
+import sys
 
 
 def msg_input(stdscr):
@@ -23,6 +25,152 @@ def msg_input(stdscr):
     return message
 
 
+def app_skeleton(stdscr):
+    curses.noecho()
+
+    # Non-blocking or cbreak mode... do not wait for Enter key to be pressed.
+    curses.cbreak()
+
+    # Turn off blinking cursor
+    curses.curs_set(False)
+
+    # Enable color if we can...
+    if curses.has_colors():
+        curses.start_color()
+
+    # Optional - Enable the keypad. This also decodes multi-byte key sequences
+    # stdscr.keypad(True)
+
+    # END ncurses startup/initialization...
+
+    caughtExceptions = ""
+    try:
+        # Meat of the program goes here.
+
+        # Below is Python's no-op directive. This is needed because a try block
+        # cannot be empty.
+        pass
+    except Exception as err:
+        # Just printing from here will not work, as the program is still set to
+        # use ncurses.
+        # print ("Some error [" + str(err) + "] occurred.")
+        caughtExceptions = str(err)
+
+    # BEGIN ncurses shutdown/deinitialization...
+    # Turn off cbreak mode...
+    curses.nocbreak()
+
+    # Turn echo back on.
+    curses.echo()
+
+    # Restore cursor blinking.
+    curses.curs_set(True)
+
+    # Turn off the keypad...
+    # stdscr.keypad(False)
+
+    # Display Errors if any happened:
+    if "" != caughtExceptions:
+        print("Got error(s) [" + caughtExceptions + "]")
+
+
+def demo_menu(stdscr):
+    k = 0
+    cursor_x = 0
+    cursor_y = 0
+
+    # Clear and refresh the screen for a blank canvas
+    stdscr.clear()
+    stdscr.refresh()
+
+    # Start colors in curses
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    # Loop where k is the last character pressed
+    while k != ord("q"):
+        # Initialization
+        stdscr.clear()
+        height, width = stdscr.getmaxyx()
+
+        if k == curses.KEY_DOWN:
+            cursor_y = cursor_y + 1
+        elif k == curses.KEY_UP:
+            cursor_y = cursor_y - 1
+        elif k == curses.KEY_RIGHT:
+            cursor_x = cursor_x + 1
+        elif k == curses.KEY_LEFT:
+            cursor_x = cursor_x - 1
+
+        cursor_x = max(0, cursor_x)
+        cursor_x = min(width - 1, cursor_x)
+
+        cursor_y = max(0, cursor_y)
+        cursor_y = min(height - 1, cursor_y)
+
+        # Declaration of strings
+        title = "Curses example"[: width - 1]
+        subtitle = "Written by Clay McLeod"[: width - 1]
+        keystr = "Last key pressed: {}".format(k)[: width - 1]
+        statusbarstr = "Press 'q' to exit | STATUS BAR | Pos: {}, {}".format(cursor_x, cursor_y)
+        if k == 0:
+            keystr = "No key press detected..."[: width - 1]
+
+        # Centering calculations
+        start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
+        start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
+        start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
+        start_y = int((height // 2) - 2)
+
+        # Rendering some text
+        whstr = "Width: {}, Height: {}".format(width, height)
+        stdscr.addstr(0, 0, whstr, curses.color_pair(1))
+
+        # Render status bar
+        stdscr.attron(curses.color_pair(3))
+        stdscr.addstr(height - 1, 0, statusbarstr)
+        stdscr.addstr(height - 1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+        stdscr.attroff(curses.color_pair(3))
+
+        # Turning on attributes for title
+        stdscr.attron(curses.color_pair(2))
+        stdscr.attron(curses.A_BOLD)
+
+        # Rendering title
+        stdscr.addstr(start_y, start_x_title, title)
+
+        # Turning off attributes for title
+        stdscr.attroff(curses.color_pair(2))
+        stdscr.attroff(curses.A_BOLD)
+
+        # Print rest of text
+        stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
+        stdscr.addstr(start_y + 3, (width // 2) - 2, "-" * 4)
+        stdscr.addstr(start_y + 5, start_x_keystr, keystr)
+        stdscr.move(cursor_y, cursor_x)
+
+        # Refresh the screen
+        stdscr.refresh()
+
+        # Wait for next input
+        k = stdscr.getch()
+
+
 if __name__ == "__main__":
-    msg = curses.wrapper(msg_input)
-    print(msg)
+    parser = ArgumentParser()
+
+    parser.add_argument("bin", type=str, help="binary to run")
+    args = parser.parse_args()
+
+    match args.bin:
+        case "msg_input":
+            msg = curses.wrapper(msg_input)
+            print("Result: " + str(msg))
+        case "skeleton":
+            result = curses.wrapper(app_skeleton)
+            print("Result: " + str(result))
+        case "demo-menu":
+            result = curses.wrapper(demo_menu)
+            print("Result: " + str(result))
